06fb0071868b1068a1cc60ae08cd851a4da51a81
Alifmslm <Alifmslm01@gmail.com>
2023-01-18 09:03:48 +0700

    operator penugasan

--- operatorp.php
+++ operatorp.php
@@ -1,5 +1,7 @@
 <?php 
 // operator aritmatika
+echo "Operator Aritmatika";
+echo "\n";
 $a = 10;
 $b = 5;
 $c = 2.5;
@@ -14,5 +16,28 @@ echo "\n";
 
 $result = $c / $a;
 var_dump($result);
+
+echo "\n";
+
+//operator penugasan
+echo "Operator Penugasan";
+echo "\n";
+
+$total = 0;
+
+$fruit = 5000;
+$chichken = 10000;
+$orangeJuice = 5000;
+
+// 5000(fruit) + tambah 0(total) = 5000
+$total += $fruit;
+// 5000(total) + 10000(chicken) = 15000
+$total += $chichken;
+// 15000(total) + 5000(orangeJuice) = 20000
+$total += $orangeJuice;
+
+echo $total;
+
+
 
 ?>
