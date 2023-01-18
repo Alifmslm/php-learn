b02e0d9bf364ed07bfbbbcbd2fef490c7752bf94
Alifmslm <Alifmslm01@gmail.com>
2023-01-18 08:53:52 +0700

    operator aritmatika

---
+++ operatorp.php
@@ -0,0 +1,18 @@
+<?php 
+// operator aritmatika
+$a = 10;
+$b = 5;
+$c = 2.5;
+
+$result = $a + $b;
+echo $result;
+echo "\n";
+
+$result = $a - $b;
+echo $result;
+echo "\n";
+
+$result = $c / $a;
+var_dump($result);
+
+?>
