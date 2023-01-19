935bdcf1b34c4b14481db6e907134ca063458d63
Alifmslm <Alifmslm01@gmail.com>
2023-01-19 08:27:24 +0700

    Data Type Conversion

---
+++ konversi.php
@@ -0,0 +1,22 @@
+<?php 
+
+echo "Konversi Angka";
+echo "\n";
+echo "\n";
+
+$valueNormal  = 100;
+var_dump($valueNormal);
+
+$valueString = (string)100;
+var_dump($valueString);
+
+$valueInt = (int)100;
+var_dump($valueInt);
+
+$valueFloat = (float)100;
+var_dump($valueFloat);
+
+$valueArray = (array)1;
+var_dump($valueArray);
+
+?>
