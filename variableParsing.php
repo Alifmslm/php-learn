6d0c691884dfaa81e775701e9003a3395313d772
Alifmslm <Alifmslm01@gmail.com>
2023-01-19 08:47:20 +0700

    variable parsing & curly brace

---
+++ variableParsing.php
@@ -0,0 +1,17 @@
+<?php 
+
+$name = "Qonita";
+
+//curly brace
+//-----
+echo "Hello {$name}'s, " . "Selamat Belajar!" . PHP_EOL;
+
+//variaable parsing
+//-----
+echo "Hello " . $name . "'s" . ", " . "Selamat Belajar!" . PHP_EOL;
+
+//nga tau yg bawah
+//-----
+echo "Hello $name's, Selamat Belajar!"
+
+?>
