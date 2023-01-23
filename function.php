43e89bc6b51cb6d7f852a570772d016e4d49fc35
Alifmslm <Alifmslm01@gmail.com>
2023-01-23 13:45:25 +0700

    function-argument

--- function.php
+++ function.php
@@ -7,4 +7,27 @@ function sayHello() {
 sayHello();
 sayHello();
 
+echo "\n";
+echo "\n";
+
+//function argument
+echo "Function Argument";
+echo "\n";
+
+function sayHello2($name) {
+    echo "Hello $name" . PHP_EOL;
+}
+
+sayHello2("Iyal");
+sayHello2("World");
+
+echo "\n";
+
+function sayHello3($firstName, $lastName) {
+    echo "Hello $firstName $lastName" . PHP_EOL;
+}
+
+sayHello3("Hasna", "Fauzi");
+sayHello3("Hasna", "Fauzi");
+
 ?>
