849628a76a1a482487c59efc9a8ebe2f6f5aa9f1
Alifmslm <Alifmslm01@gmail.com>
2023-01-23 14:01:20 +0700

    function default argument & text declaration

--- function.php
+++ function.php
@@ -30,4 +30,33 @@ function sayHello3($firstName, $lastName) {
 sayHello3("Hasna", "Fauzi");
 sayHello3("Hasna", "Fauzi");
 
+echo "\n";
+
+//function default argument
+echo "Function default argument";
+echo "\n";
+echo "\n";
+
+function sayHello4($name = "Niki") {
+    echo "Hello $name" . PHP_EOL;
+}
+
+sayHello4();
+
+echo "\n";
+
+//text declaration
+echo "Text Declaration";
+echo "\n";
+echo "\n";
+
+function sum(int $first, int $last) {
+    $total = $first + $last;
+    echo "Hasil dari $first + $last = $total" . PHP_EOL;
+}
+
+sum(100, 100);
+sum("100", "100");
+sum(true, false)
+
 ?>
